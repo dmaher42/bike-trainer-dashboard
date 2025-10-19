56793925a8fdd710b9c0c0a82150ee5dcadb7c5f
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2025-10-20 09:02:31 +1030

    Update week 2 lesson activities

---
+++ src/utils/lessonActivities.ts
@@ -0,0 +1,15 @@
+import { LessonActivitiesWeek } from "../types";
+
+export const lessonActivities: LessonActivitiesWeek[] = [
+  {
+    week: 2,
+    activities: [
+      "Character mapping",
+      "Family tree",
+    ],
+    notes:
+      "Replaced prior evidence analysis drills with collaborative character-focused work to deepen narrative comprehension.",
+  },
+];
+
+export default lessonActivities;
