51af40e203b90acfceaf549b81b1e400506afe08
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2025-10-20 16:45:02 +1030

    Configure Tailwind with custom neutral palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
         neutral: {
-          950: "#0a0a0a",
-          900: "#171717",
-          800: "#262626",
-          700: "#404040",
-          500: "#737373",
-          400: "#a3a3a3",
-          300: "#d4d4d4",
-          200: "#e5e5e5",
-          100: "#f5f5f5",
-        },
-      },
+          950: '#0a0a0a',
+          900: '#171717',
+          800: '#262626',
+          700: '#404040',
+          500: '#737373',
+          400: '#a3a3a3',
+          300: '#d4d4d4',
+          200: '#e5e5e5',
+          100: '#f5f5f5',
+        }
+      }
     },
   },
   plugins: [],
-};
+}
